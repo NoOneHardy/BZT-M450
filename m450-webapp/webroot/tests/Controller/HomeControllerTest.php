2b8cd110be2b47df228f70577203572754deba07
Silas Noah Hardegger <silas.hardegger@outlook.com>
2024-10-31 06:12:26 +0100

    tests

--- m450-webapp/webroot/tests/Controller/HomeControllerTest.php
+++ m450-webapp/webroot/tests/Controller/HomeControllerTest.php
@@ -5,16 +5,18 @@ namespace Test\Controller;
 use App\Controller\HomeController;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 
-class HomeControllerTest extends TestCase {
-    public function testInputParamsWithValues() {
+class HomeControllerTest extends TestCase
+{
+    public function testInputParamsWithValues()
+    {
         $controller = new HomeController();
         $sri = $this->createStub(ServerRequestInterface::class);
         $sri->method('getQueryParams')->willReturn([
-            'getQueryParams' => [
-                'zip' => '8000',
-                'date' => 'date'
-            ]
+            'zip' => '8000',
+            'date' => 'date'
         ]);
 
         $result = $controller->getQueryParams($sri);
@@ -23,5 +25,38 @@ class HomeControllerTest extends TestCase {
         $this->assertArrayHasKey('zip', $result);
         $this->assertArrayHasKey('mode', $result);
         $this->assertEquals('historic', $result['mode']);
+        $this->assertEquals('8000', $result['zip']);
+        $this->assertEquals('date', $result['date']);
+    }
+
+    public function testGetWeatherDataHTML()
+    {
+        $req = $this->createStub(ServerRequestInterface::class);
+        $req->method('getQueryParams')->willReturn([
+            'zip' => '8000',
+            'date' => 'date'
+        ]);
+        $res = $this->createStub(ResponseInterface::class);
+        $bodyStream = $this->createStub(StreamInterface::class);
+        $res->method('getBody')->willReturn($bodyStream);
+
+        $controller = $this->getMockBuilder(HomeController::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods([
+                'getQueryParams',
+                'getHistoricData',
+                'getHistoricWeatherData',
+                'getHistoricAirData',
+                'getCurrentData',
+                'getCurrentWeatherData',
+                'normalizeWeatherData',
+                'getCurrentAirData',
+                'normalizeAirData'
+            ])->getMock();
+
+        $controller->expects($this->once())->method('getQueryParams')->with($req);
+        $result = $controller->getWeatherDataHtml($req, $res, []);
+
+        $this->assertInstanceOf(ResponseInterface::class, $result);
     }
 }
